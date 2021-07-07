91b4ea9cf38ba39ba01d2db88e838eb0fb40fce2
Karunesh Tripathi <karunesh.tripathi2019@vitstudent.ac.in>
2021-07-07 14:43:56 +0530

    make it better

---
+++ client/src/components/Projects/data.js
@@ -0,0 +1,69 @@
+import photo1 from "./portfolio.png"
+import photo2 from "./versioncontrol.png"
+import photo3 from "./chatapp.png"
+import photo4 from "./chatapp1.png"
+import photo5 from "./enron.png"
+import photo6 from "./homoeo.png"
+import photo7 from "./hdfs.png"
+import photo8 from "./college.png"
+import photo9 from "./bst.png"
+
+const data={
+    "projects":[
+        {
+            "id":1,
+            "photo":photo1,
+            "text":"Scroll Through Porfolio Website using ReactJS",
+            "link":"#Home",
+        },
+        {
+            "id":2,
+            "photo":photo2,
+            "text":"Version Control System with UML Diagram Maker using ReactJS,MongoDB,Express and NodeJS",
+            "link":"https://version-control-and-uml.herokuapp.com/",
+        },
+        {
+            "id":3,
+            "photo":photo6,
+            "text":"Responsive Website for Doctor's Clinic using HTML,CSS and JS",
+            "link":"https://karuneshtripathi13.github.io/Homoeocare.github.io/",
+        },
+        {
+            "id":4,
+            "photo":photo3,
+            "text":"Realtime, Crossplatform Group Chat Application Using React Native and Firebase using expo-cli",
+            "link":"https://github.com/karuneshtripathi13/ChatApp.git",
+        },
+        {
+            "id":5,
+            "photo":photo5,
+            "text":"Analysed Enron Email Dataset and trained Supervised and Unsupervised Machine Learning models to identify Person of Interest",
+            "link":"https://github.com/karuneshtripathi13/Machine-Learning-Project.git",
+        },
+        {
+            "id":6,
+            "photo":photo4,
+            "text":"Created Realtime Unicast(point-to-point communication) Chat Application Using React Native and Firebase",
+            "link":"https://github.com/karuneshtripathi13/Unicast-Chat-App.git",
+        },
+        {
+            "id":7,
+            "photo":photo7,
+            "text":"Created small scale HDFS using C in linux environment and resolved small file problem in it",
+            "link":"https://github.com/karuneshtripathi13/HDFS-Implementaion.git",
+        },
+        {
+            "id":8,
+            "photo":photo8,
+            "text":"A Stand-Alone Application of college management system using JavaFX and MySQL",
+            "link":"https://github.com/karuneshtripathi13/College-Management.git",
+        },
+        {
+            "id":9,
+            "photo":photo9,
+            "text":"Hostel Management DBMS using Binary Search Tree algorithm using C",
+            "link":"https://github.com/karuneshtripathi13/BST-DBMS",
+        },
+    ]
+}
+export default data;
